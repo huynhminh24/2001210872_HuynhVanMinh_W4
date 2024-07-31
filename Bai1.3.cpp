60ca7350832e3935ada6a7c8bd16a2de6a539fa5
huynhminh24 <manager.hvm@gmail.com>
2024-07-31 16:52:32 +0700

    Bai1.3 complete

---
+++ Bai1.3.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+double sumN(int n)
+{
+    if (n == 1) {
+        return 1.0 / 2.0;
+    }
+    else {
+        return sumN(n - 1) + (double)n / (n + 1);
+    }
+}
+
+int main() {
+    int n;
+    printf("Nhap n: ");
+    scanf_s("%d", &n);
+    printf("Tong bang: %.6f\n", sumN(n));
+    return 0;
+}

d17777b177f3d4b5a99ea2d4a8e806b6afcfe020
huynhminh24 <manager.hvm@gmail.com>
2024-08-05 11:53:57 +0700

    Bai1.5 complete

---
+++ Bai1.5.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+double sumSeries(int n) {
+    double sum = 0.0;
+    for (int i = 1; i <= n; i++) {
+        sum += i * (i + 1);
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    printf("Nhap n: ");
+    scanf_s("%d", &n);
+    printf("Tong bang: %.6f\n", sumSeries(n));
+    return 0;
+}
